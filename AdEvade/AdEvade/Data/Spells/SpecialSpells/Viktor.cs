4c13190f5ea3a59ec19390abdcdc5e99b6c12471
FireBuddy <aijun@live.ca>
2016-07-17 18:51:16 -0400

    Update Viktor.cs

--- AdEvade/AdEvade/Data/Spells/SpecialSpells/Viktor.cs
+++ AdEvade/AdEvade/Data/Spells/SpecialSpells/Viktor.cs
@@ -32,7 +32,7 @@ namespace AdEvade.Data.Spells.SpecialSpells
                 args.SData.Name != null && args.SData.Name == "ViktorDeathRay")
             
             {
-               . var End = new vector3() => sender.GetWaypoints().Last();
+               . var End = new vector3 => sender.GetWaypoints().Last();
             //    var missileDist = End.To2D().Distance(args.Start.To2D());
               //  var delay = missileDist / 1.5f + 600;
         
